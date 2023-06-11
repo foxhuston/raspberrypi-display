6d6a3734d18743d8394d35636d539d6353618fff
Fox Huston <448633+foxhuston@users.noreply.github.com>
2023-06-11 08:32:21 -0400

    wired together some libraries

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::error::Error;
+
+use rppal::i2c::I2c;
+// use rppal::system::DeviceInfo;
+
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
+
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let i2c = I2c::new()?;
+
+    println!("Hello, world!");
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+        let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
+    Ok(())
+}
