ab958c007becc777b26e64165554f63da1d63829
Fox Huston <fox.huston@tufts.edu>
2023-06-11 10:02:38 -0400

    Added simulator code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fmt::Debug};
 
 use rppal::i2c::I2c;
 // use rppal::system::DeviceInfo;
@@ -7,6 +7,7 @@ use rppal::i2c::I2c;
 use embedded_graphics::{
     mono_font::{ascii::FONT_10X20, MonoTextStyle, ascii::FONT_6X10},
     pixelcolor::BinaryColor,
+    draw_target::DrawTarget,
     prelude::*,
     text::{Baseline, Text, LineHeight, TextStyleBuilder},
 };
@@ -16,18 +17,9 @@ use embedded_graphics_simulator::{BinaryColorTheme, SimulatorDisplay, Window, Ou
 
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 
-
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let i2c = I2c::new()?;
-
-    let interface = I2CDisplayInterface::new(i2c);
-    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
-    display.init().unwrap();
-
-    // let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(128, 64));
-
+fn draw<D>(display: &mut D) -> Result<(), D::Error>
+    where D: DrawTarget<Color = BinaryColor>, <D as DrawTarget>::Error : Debug
+{
     let character_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
 
     let text_style = TextStyleBuilder::new()
@@ -35,14 +27,50 @@ fn main() -> Result<(), Box<dyn Error>> {
         .build();
 
     Text::with_text_style("Hello world!", Point::zero(), character_style, text_style)
-        .draw(&mut display)
+        .draw(display)
         .unwrap();
 
     Text::with_text_style("Hello Rust!", Point::new(0, 16), character_style, text_style)
-        .draw(&mut display)
+        .draw(display)
         .unwrap();
 
+    Ok(())
+}
+
+#[cfg(feature = "embedded-graphics-simulator")]
+fn run_display() -> Result<(), Box<dyn Error>> {
+    let mut display = SimulatorDisplay::<BinaryColor>::new(Size::new(128, 64));
+
+    draw(&mut display)?;
+
+    let output_settings = OutputSettingsBuilder::new()
+        .theme(BinaryColorTheme::OledBlue)
+        .build();
+
+    Window::new("Hello World", &output_settings).show_static(&display);
+
+
+    Ok(())
+}
+
+#[cfg(not(feature = "embedded-graphics-simulator"))]
+fn run_display() -> Result<(), Box<dyn Error>> {
+    let i2c = I2c::new()?;
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    draw(&mut display);
+
     display.flush().unwrap();
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    run_display()?;
 
     Ok(())
 }
